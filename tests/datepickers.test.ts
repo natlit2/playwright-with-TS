66c7df0d65639585b51fd6b9432ad0cf1f3313f6
anton litvak <anton.k.litvak@gmail.com>
2022-12-14 13:33:38 +0100

    wraping to reusable selectDate function, datepicker toggle condition past, present, future and selection

--- tests/datepickers.test.ts
+++ tests/datepickers.test.ts
@@ -16,35 +16,44 @@ test("handling datepickers using moment ", async ({ page }) => {
     "https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo"
   );
 
-  await page.click("//input[@placeholder='Start date']");
-
   //find and define all the locators needed for toggling the calendar
+  //past
+  await selectDate(12, "March 2021");
+  await page.reload();
+  //future
+  await selectDate(11, "July 2023");
+  await page.reload();
+  //present
+  await selectDate(14, "December 2022");
+  await page.reload();
 
-  const mmYY = page.locator(
-    "(//table[@class='table-condensed']//th[@class='datepicker-switch'])[1]"
-  );
-  const prev = page.locator(
-    "//div[@class='datepicker-days']//table[@class='table-condensed']//th[@class='prev']"
-  );
-  const next = page.locator(
-    "//div[@class='datepicker-days']//table[@class='table-condensed']//th[@class='next']"
-  );
+  async function selectDate(date: number, dateToSelect: string) {
+    await page.click("//input[@placeholder='Start date']");
+    const mmYY = page.locator(
+      "(//table[@class='table-condensed']//th[@class='datepicker-switch'])[1]"
+    );
+    const prev = page.locator(
+      "//div[@class='datepicker-days']//table[@class='table-condensed']//th[@class='prev']"
+    );
+    const next = page.locator(
+      "//div[@class='datepicker-days']//table[@class='table-condensed']//th[@class='next']"
+    );
 
-  //toggle to a pre-defined month and year
+    //toggle to a pre-defined month and year
+    // let dateToSelect: string = "March 2022";
 
-  let dateToSelect: string = "March 2022";
+    // check if the current month is before or after the predefined date using momentJS and toggle the calendar accordingly
+    const thisMonth = moment(dateToSelect, "MMMM YYYY").isBefore();
+    console.log("this month?" + thisMonth);
 
-  // check if the current month is before or after the predefined date using momentJS and toggle the calendar accordingly
-  const thisMonth = moment(dateToSelect, "MMMM YYYY").isBefore();
-  console.log("this month?" + thisMonth);
-
-  while ((await mmYY.textContent()) != dateToSelect) {
-    if (thisMonth) {
-      await prev.click();
-    } else {
-      await next.click();
+    while ((await mmYY.textContent()) != dateToSelect) {
+      if (thisMonth) {
+        await prev.click();
+      } else {
+        await next.click();
+      }
     }
+    //finally click on the desired day to select
+    await page.click(`//td[@class='day'][text()='${date}']`);
   }
-  //finally click on the desired day to select
-  await page.click("//td[@class='day'][text()='3']");
 });
