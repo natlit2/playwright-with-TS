d821234ffe34429d991013e47827fb7ff6d08cc5
anton litvak <anton.k.litvak@gmail.com>
2022-12-14 13:16:27 +0100

    added test to toggle the calendar and select a data with prefrences

--- tests/datepickers.test.ts
+++ tests/datepickers.test.ts
@@ -1,15 +1,15 @@
 import { expect, test } from "@playwright/test";
+import moment from "moment";
+// test("Handling bootstrap date picker", async ({ page }) => {
+//   await page.goto(
+//     "https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo"
+//   );
+//   let date = "1988-02-08";
 
-test("Handling bootstrap date picker", async ({ page }) => {
-  await page.goto(
-    "https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo"
-  );
-  let date = "1988-02-08";
+//   await page.fill("id=birthday", date);
 
-  await page.fill("id=birthday", date);
-
-  //await page.waitForTimeout(3000);
-});
+//   //await page.waitForTimeout(3000);
+// });
 
 test("handling datepickers using moment ", async ({ page }) => {
   await page.goto(
@@ -18,13 +18,33 @@ test("handling datepickers using moment ", async ({ page }) => {
 
   await page.click("//input[@placeholder='Start date']");
 
+  //find and define all the locators needed for toggling the calendar
+
   const mmYY = page.locator(
     "(//table[@class='table-condensed']//th[@class='datepicker-switch'])[1]"
   );
   const prev = page.locator(
-    "(//table[@class='table-condensed']//th[@class='datepicker-prev'])[1]"
+    "//div[@class='datepicker-days']//table[@class='table-condensed']//th[@class='prev']"
   );
   const next = page.locator(
-    "(//table[@class='table-condensed']//th[@class='datepicker-next'])[1]"
+    "//div[@class='datepicker-days']//table[@class='table-condensed']//th[@class='next']"
   );
+
+  //toggle to a pre-defined month and year
+
+  let dateToSelect: string = "March 2022";
+
+  // check if the current month is before or after the predefined date using momentJS and toggle the calendar accordingly
+  const thisMonth = moment(dateToSelect, "MMMM YYYY").isBefore();
+  console.log("this month?" + thisMonth);
+
+  while ((await mmYY.textContent()) != dateToSelect) {
+    if (thisMonth) {
+      await prev.click();
+    } else {
+      await next.click();
+    }
+  }
+  //finally click on the desired day to select
+  await page.click("//td[@class='day'][text()='3']");
 });
